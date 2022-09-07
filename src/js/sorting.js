ca8c8ae4b0d5bc9062410da4ade51a44e08c1d6d
Josue <63822871+mycodingstuff@users.noreply.github.com>
2022-09-07 10:31:08 -0600

    enhancements
    enhancements

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -9,13 +9,35 @@ function getHigherNumberSorting(elements) {
 }
 
 function getHigherNumberTraversing(elements) {
-    let max = elements[0]
+    let max = elements[0];
+
     for (let i = 0; i < elements.length; i++) {
         if (max < elements[i]) {
-            max = elements[i]
+            max = elements[i];
         }
     };
+
     return max;
+}
+
+function benchMarkFunction(callback){
+    let startTime = performance.now();
+    callback();
+    let endTime = performance.now();
+
+    return (endTime - startTime).toFixed(4);
+}
+
+function addSuccessMarkTo(target){
+    target.html(`<div class="success-checkmark">
+    <div class="check-icon">
+      <span class="icon-line line-tip"></span>
+      <span class="icon-line line-long"></span>
+      <div class="icon-circle"></div>
+      <div class="icon-fix"></div>
+    </div>
+  </div>
+  <center>Better performance, faster computation</center>`);
 }
 
 $(document).ready(() => {
@@ -25,7 +47,16 @@ $(document).ready(() => {
     let mainContainerElements = $("#main-container-elements");
     let mainContainerComputedResult = $("#main-container-computed-result");
     let mainContainerComputedWrapper = $("#main-container-computed-wrapper");
+    let traverseAlgorithmExecutionTime = $(".traverse-algorithm-execution-time");
+    let sortingAlgorithmExecutionTime = $(".sorting-algorithm-execution-time");
+    let traverseAlgorithmSuccessMark = $("#traverse-algorithm-success-mark");
+    let sortingAlgorithmSuccessMark = $("#sorting-algorithm-success-mark");
     let generatedRandomElements = [];
+
+     $("#traverse-algorithm-code")
+        .html(getHigherNumberTraversing.toString());    
+     $("#sorting-algorithm-code")
+        .html(getHigherNumberSorting.toString());    
 
     buttonGenerate.click(() => {
 
@@ -33,7 +64,7 @@ $(document).ready(() => {
         let totalElements = randomSetSize.val();
 
         for (var i = 0; i <= totalElements; i++) {
-            generatedRandomElements.push(getRandomInt(1000));
+            generatedRandomElements.push(getRandomInt(100000));
         }
 
         mainContainerElements.html(`<div><span>${generatedRandomElements.join('</span></div><div><span>')}</span></div>`);
@@ -42,9 +73,25 @@ $(document).ready(() => {
 
 
     mainContainerComputedWrapper.click(() => {
-        console.log(getHigherNumberSorting(generatedRandomElements));
-        console.log(getHigherNumberTraversing(generatedRandomElements));
 
+        let sortingTime = benchMarkFunction(()=>{
+            getHigherNumberSorting(generatedRandomElements)
+        });
+
+        sortingAlgorithmExecutionTime.text("Elapsed time: "+sortingTime+"ms") ;        
+
+        let traverseTime = benchMarkFunction(()=>{
+            getHigherNumberTraversing(generatedRandomElements)
+        });
+
+        traverseAlgorithmExecutionTime.text("Elapsed time: "+ traverseTime +"ms");
+
+        if(sortingTime < traverseTime){
+            addSuccessMarkTo(sortingAlgorithmSuccessMark);
+        }else{
+            addSuccessMarkTo(traverseAlgorithmSuccessMark);
+        }
+        
         mainContainerComputedResult.text(`The highest number is: ${getHigherNumberSorting(generatedRandomElements)} `);
     });
 
